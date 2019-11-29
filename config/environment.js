0c79b4413f61a313ff57a4c9812bfb1ad8e90d7c
amirullah23 <amirullah.kasang@gmail.com>
2019-11-29 14:44:05 +0700

    add port

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 
 module.exports = {
-    PORT: process.env.PORT, 
+    PORT: 5000 || process.env.PORT, 
     DATABASE: process.env.DATABASE ,
     DATABASE_NAME: process.env.DATABASE_NAME 
 
